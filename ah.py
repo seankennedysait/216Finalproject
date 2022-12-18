dac9581af4fc4d921b502c27570396f612c10ba0
Sukhjinder Kahlon <sukhjindersingh.kahlon@edu.sait.ca>
2022-12-17 20:17:13 -0700

    Bug fixes

--- ah.py
+++ ah.py
@@ -364,6 +364,20 @@ class Management:
             else:
                 assert (False)
 
+    def writeToFiles(self, files:dict):
+        """ Writes the data in the self.__lists to files """
+        for key in files:
+            if (key == "doctor"):
+                Doctor.writeListOfDoctorsToFile(self.__lists[key], files[key]) 
+            elif (key == "facility"):
+                Facility.writeListOfFacilitiesToFile(self.__lists[key], files[key])
+            elif (key == "lab"):
+                Laboratory.writeListOfLabsToFile(self.__lists[key], files[key])
+            elif (key == "patient"):
+                Patient.writePatientsListToFile(self.__lists[key], files[key])
+            else:
+                assert (False)
+
     def main_menu(self):
         menu = int(9)
         while (menu!=0):
@@ -415,10 +429,13 @@ class Management:
                 else:
                     doc.displayDoctorInfo()
             elif (menu == 4):
-                self.__lists["doctor"].append(Doctor.enterDrInfo())
+                new_doc = Doctor.enterDrInfo()
+                self.__lists["doctor"].append(new_doc)
+                new_doc.addDrToFile(self.__filenames["doctor"])
             elif (menu == 5):
                 ID = int(input("Enter the id of the Doctor whose information you want to edit: "))
                 Doctor.editDoctorInfo(ID, self.__lists["doctor"])
+                Doctor.writeListOfDoctorsToFile(self.__lists["doctor"], self.__filenames["doctor"])
             elif(menu == 6):
                 break
             else:
@@ -438,7 +455,9 @@ class Management:
             if (menu == 1):
                 Facility.displayFacilities(self.__lists["facility"])
             elif (menu == 2):
-                self.__lists["facility"].append(Facility.enterFacilityInfo())
+                new_fac = Facility.enterFacilityInfo()
+                self.__lists["facility"].append(new_fac)
+                new_fac.addFacility(self.__filenames["facility"])
             elif (menu == 3):
                 break
             else:
@@ -458,7 +477,9 @@ class Management:
             if (menu == 1):
                 Laboratory.displayLabsList(self.__lists["lab"])
             elif (menu == 2):
-                self.__lists["lab"].append(Laboratory.enterLaboratoryInfo())
+                new_lab = Laboratory.enterLaboratoryInfo()
+                self.__lists["lab"].append(new_lab)
+                new_lab.addLabToFile(self.__filenames["lab"])
             elif (menu == 3):
                 break
             else:
@@ -487,10 +508,13 @@ class Management:
                 else:
                     pat.displayPatientInfo()
             elif (menu == 3):
-                self.__lists["patient"].append(Patient.enterPatientInfo())
+                new_pat = Patient.enterPatientInfo()
+                self.__lists["patient"].append(new_pat)
+                new_pat.addPatientToFile(self.__filenames["patient"])
             elif (menu == 4):
                 pid = int(input("Enter the Id of patient whose info you want to edit: "))
                 Patient.editPatientInfo(pid, self.__lists["patient"])
+                Patient.writePatientsListToFile(self.__lists["patient"], self.__filenames["patient"])
             elif (menu == 5):
                 break
             else:
